eb51edfe92f02ef89f6d19d5afffdc026359a155
nuthan111 <142194638+nuthan111@users.noreply.github.com>
2024-12-06 13:37:13 +0530

    Delete audio.py

--- audio.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-from playsound import playsound
-audio="speech.mp3"
-language="en"
-sp=gTTS(text="my name is nuthan kumar",lang=language,slow=False)
-sp.save(audio)
-playsound(audio)
-print("=====audio is playing======")
